f6108c0ebfc530b50955cd76858cbcbd9e4fbffb
trevor oakley <trevoroakley200@gmail.com>
2019-08-16 06:45:18 +0100

    setup

--- src/main/java/BlockgetStart.java
+++ src/main/java/BlockgetStart.java
@@ -1,6 +1,8 @@
 import ch.decent.sdk.model.Account;
 import ch.decent.sdk.model.NftData;
 import ch.decent.sdk.model.NftModel;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 
 import java.io.*;
 import java.util.List;
@@ -10,13 +12,30 @@ import java.util.regex.PatternSyntaxException;
 public class BlockgetStart {
 
 
-    public static void main(String[] args) throws IOException {
-       BlockgetAccount anAccount = new BlockgetAccount();
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+     //   BlockgetRpc server = new BlockgetRpc();
+     //   server.start();
+     //   server.blockUntilShutdown();
+
+        System.out.println("running");
+        Server server1 = ServerBuilder.forPort(50051).build();
+        server1.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread( () -> {
+            System.out.println("shutting down");
+            server1.shutdown();
+            System.out.println("Stopped Server");
+        } ));
+        server1.awaitTermination();
+
+        BlockgetAccount anAccount = new BlockgetAccount();
         Account myAcct = anAccount.getAccountByName("trevor3");
        System.out.println("hellow from blockget");
        System.out.println("registrar acct = " + myAcct.getRegistrar());
 
        String hashcode = "QmZfPNDiujH2KFuJhB47tAdup2ZY54611wiH6i15ZwTipa";
+       String hashcode1 = "QmaKML4J1rajBvNQRZo7Ww4hnQMvspbzUp1VaPEC8SAmzX";
         BlockgetNftMgr bnm = new BlockgetNftMgr();
         String accountName = "trevor3";
         List<NftData<? extends NftModel>> result=bnm.ConfirmNFT(myAcct);
@@ -26,7 +45,7 @@ public class BlockgetStart {
         int g=1;
         //bnm.create("BGT"); // blockget token
 
-       // bnm.issue("BGT", hashcode);
+       // bnm.issue("BGT", hashcode1);
 
 
      //   System.out.println("name acct = " + myAcct.getName());
